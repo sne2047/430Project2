28eefc4fcb61677389050e063b98b5d2b07c6c48
sne2047 <sne2047@rit.edu>
2021-11-12 09:25:31 -0500

    trying to get npm stuff working. It's not.

---
+++ server/router.js
@@ -0,0 +1,8 @@
+const controllers = require('./controllers');
+const mid = require('./middleware');
+
+const router = (app) => {
+    //route stuff here, app.post and app.get things
+}
+
+module.exports = router;
