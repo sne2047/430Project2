c3d700c553b47a5474448ae8aa4932631df7b7d4
sne2047 <sne2047@rit.edu>
2021-11-17 12:40:20 -0500

    don't remember when I last committed- added some scripts so can do npm start, though it doesn't work yet. Added some handlebars pages, though again, not done. Also need to add redis to app.js still.

--- README.txt
+++ README.txt
@@ -1,10 +1,21 @@
 This is my project 2 for IGME 430.
 Use this document to keep track of plans, bugs i have to deal with but are tabling for now, etc
 
-Current task: Get the basics of creating an account and logging into one copied from previous
-First step of that is getting the controllers and models for accounts set up.
-And also App.js needs to be done
-Then some basic middleware and account stuff
-and then some of the html pages for these basics
+Current task: 
+Get the basics of creating an account and logging into one copied from previous.
+Components to this task:
+and some of the html pages for these basics
+App.js get redis set up properly and all redis associated stuff
+set up the router
+Add stylesheets under /assets/style.css
+replace placeholder images
+sections for react to play with in main.handlebars
+
+Done:
+Account controller and models
+Account is in index.js
+Most of app.js, minus the redis
+Added a favicon
+middleware set up
 
 note- instead of /maker, send people to /dragons, the page that will show the user their created dragons
--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-const path = requie('path');
+const path = require('path');
 const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
@@ -11,3 +11,58 @@ const RedisStore = require('connect-redis')(session);
 const url = require('url');
 const csrf = require('csurf');
 const redis = require('redis');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/Project2';
+//this may be totally wrong for the local one, check again later when it's complete enough for testing
+
+mongoose.connect(dbURL, (err) => {
+    if (err) {
+        console.log('Could not connect to database.');
+        throw err;
+    }
+});
+
+//redis stufffffff
+//Skipped for now cause getting redis properly set up for this w/o breaking domomaker...
+//yeaaaah tiring, worn out.
+//also there's more redis stuff a bit further down. be sure to get that too.
+
+//bring in our router
+const router = require('./router.js');
+
+const app = express();//app
+//get the app set up
+app.use('/assets',express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
+app.use(compression());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+//more redis stuffffff
+//needs to be put here
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
+app.use(cookieParser());
+//csrf needs to be in this location, after cookie parser and before the router
+app.use(csrf());
+app.use((err, req, res, next) => {
+    if(err.code !== 'EBADCSRFTOKEN') return next(err);
+    console.log('Missing CSRF token.');
+    return false;
+});
+
+//aaaand finally hook up the router
+router(app);
+
+app.listen(port, (err) => {
+    if(err){
+        throw err;
+    }
+
+    console.log(`Listening on port ${port}`);
+});
+
